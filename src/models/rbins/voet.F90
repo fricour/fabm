30c66ad324be29e4d6fa2b029b5161d14b398a80
fricour <endnoteenjoyer338@aleeas.com>
2024-02-14 09:54:39 +0100

    test simple P/D model

--- src/models/rbins/voet.F90
+++ src/models/rbins/voet.F90
@@ -1,7 +1,6 @@
 #include "fabm_driver.h"
 
 module rbins_voet
-
    use fabm_types
 
    implicit none
@@ -9,21 +8,45 @@ module rbins_voet
    private
 
    type, extends(type_base_model), public :: type_rbins_voet
-      ! Add variable identifiers and parameters here.
+      type (type_state_variable_id) :: id_p, id_d
+      real(rk) :: k_f, k_d
    contains
       procedure :: initialize
-      ! Reference model procedures here.
+      procedure :: do
    end type
 
 contains
 
-   subroutine initialize(self, configunit)
-      class (type_rbins_voet), intent(inout), target :: self
-      integer,                          intent(in)            :: configunit
- 
-      ! Register model parameters and variables here.
-   end subroutine initialize
+  subroutine initialize(self, configunit)
+    class (type_rbins_voet), intent(inout), target :: self
+    integer,                  intent(in)            :: configunit
 
-   ! Add model subroutines here.
+    call self%register_implemented_routines((/source_do/))
+
+    call self%get_parameter(self%k_f, 'k_f', 'day-1', 'fixation rate', default=0.2_rk)
+    call self%get_parameter(self%k_d, 'k_d', 'day-1', 'dissolution rate', default=0.3._rk)
+
+    call self%register_state_variable(self%id_p, 'p', 'mmolC m-3', 'particulate concentration', minimum=0.0_rk) 
+    call self%register_state_variable(self%id_d, 'd', 'mmolC m-3', 'dissolved concentration', minimum=0.0_rk)
+    
+  end subroutine initialize
+
+  subroutine do(self, _ARGUMENTS_DO_)
+    class (type_rbins_voet), intent(in) :: self
+    _DECLARE_ARGUMENTS_DO_
+
+    real(rk) :: p, d
+
+    _LOOP_BEGIN_
+        ! Obtain concentration of nutrients
+        _GET_(self%id_p, p)
+        _GET_(self%id_d, d)
+
+        ! Send rates of change to FABM.
+        _ADD_SOURCE_(self%id_p, -self%k_d*p + self%k_f*d)
+        _ADD_SOURCE_(self%id_d, -self%k_f*d + self%k_d*p)
+
+        _LOOP_END_
+  end subroutine do
 
 end module

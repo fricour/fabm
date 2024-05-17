6f0c88f284cc4054ef7e6422212cb5bad96de4f4
fricour <fricour@naturalsciences.be>
2024-05-17 15:57:12 +0200

    update dp model based on cofabm_Model

--- src/models/rbins/base_model.F90
+++ src/models/rbins/base_model.F90
@@ -8,9 +8,10 @@ module rbins_base_model
    private
 
    type, extends(type_base_model), public :: type_rbins_base_model ! it is a coincidence that I chose the same base_model as the type_base_model but it's not at all related...
-      type (type_state_variable_id) :: id_p, id_d
-      real(rk) :: k_f
-   contains
+      type (type_state_variable_id) :: id_p, id_d 
+      type (type_dependency_id) :: id_I_0
+      real(rk) :: k_p, k_p_par_n
+   contains      
       procedure :: initialize
       procedure :: do
    end type
@@ -21,13 +22,21 @@ contains
     class (type_rbins_base_model), intent(inout), target :: self
     integer,                  intent(in)            :: configunit
 
-    call self%register_implemented_routines((/source_do/))
+    real(rk), parameter :: secs_per_day = 86400.0_rk
+    real(rk), parameter :: hours_per_day = 3600.0_rk 
+    real(rk) :: w_p ! sinking speed
 
-    call self%get_parameter(self%k_f, 'k_f', 'day-1', 'fixation rate', default=0.2_rk, scale_factor=1.0_rk/86400.0_rk)
+    ! parameters
+    call self%get_parameter(self%k_p, 'k_p', 'h-1', 'precipitation rate', default=0.006_rk, scale_factor=1.0_rk/hours_per_day)
+    call self%get_parameter(self%k_p_par_n, 'k_p_par_n', '[h-1/(mumol photons/m^2/s)]', 'linear PAR dependency of precipitation rate', default=0.00000176_rk, scale_factor=1.0_rk/hours_per_day)
+    call self%get_parameter(w_p, 'w_p', 'm day-1', 'vertical velocity (<0 for sinking)', default=-1.0_rk, scale_factor=1.0_rk/secs_per_day)
 
-    call self%register_state_variable(self%id_p, 'p', 'mmolC m-3', 'particulate concentration', initial_value=200.0_rk, minimum=0.0_rk) 
+    ! state variables 
+    call self%register_state_variable(self%id_p, 'p', 'mmolC m-3', 'particulate concentration', initial_value=200.0_rk, minimum=0.0_rk, vertical_movement=w_p) 
     call self%register_state_variable(self%id_d, 'd', 'mmolC m-3', 'dissolved concentration', minimum=0.0_rk)
     
+    ! dependencies
+    call self%register_dependency(self%id_I_0, standard_variables%downwelling_photosynthetic_radiative_flux) ! units in W/m^2 
   end subroutine initialize
 
   subroutine do(self, _ARGUMENTS_DO_)
@@ -35,16 +44,19 @@ contains
     _DECLARE_ARGUMENTS_DO_
 
     real(rk) :: d
+    real(rk) :: I_0
+    real(rk) :: conversion_factor = 4.6_rk ! conversion factor between W/m^2 and micromol photon/m^2/s  
 
     _LOOP_BEGIN_
-        ! Obtain concentration of nutrients
         _GET_(self%id_d, d)
+        _GET_(self%id_I_0, I_0)
 
         ! Send rates of change to FABM.
-        _ADD_SOURCE_(self%id_p, self%k_f*d)
-        _ADD_SOURCE_(self%id_d, -self%k_f*d)
+        !_ADD_SOURCE_(self%id_p, (self%k_p*d + self%k_p_par_n*I_0)*d)
+        _ADD_SOURCE_(self%id_p, (self%k_p + self%k_p_par_n*I_0*conversion_factor)*d) 
+        _ADD_SOURCE_(self%id_d, -self%k_p*d)
 
-        _LOOP_END_
+    _LOOP_END_
   end subroutine do
 
 end module
--- src/models/rbins/dissolution.F90
+++ src/models/rbins/dissolution.F90
@@ -9,7 +9,7 @@ module rbins_dissolution
 
    type, extends(type_base_model), public :: type_rbins_dissolution
       type (type_state_variable_id) :: id_p, id_d
-      real(rk) :: k_d
+      real(rk) :: k_d 
    contains
       procedure :: initialize
       procedure :: do
@@ -21,14 +21,12 @@ contains
     class (type_rbins_dissolution), intent(inout), target :: self
     integer,                  intent(in)            :: configunit
 
-    call self%register_implemented_routines((/source_do/))
-
     call self%get_parameter(self%k_d, 'k_d', 'day-1', 'dissolution rate', default=0.3_rk, scale_factor=1.0_rk/86400.0_rk)
-
-    ! we create a dependency for a coupling in fabm.yaml with the "base_model"
-    call self%register_state_dependency(self%id_p, 'p', 'mmolC m-3', 'particulate concentration')
-    call self%register_state_dependency(self%id_d, 'd', 'mmolC m-3', 'dissolved concentration')
     
+    ! we create a dependency for a coupling in fabm.yaml with the "base_model"
+    call self%register_state_dependency(self%id_p, 'p', 'mmolC m-3', 'particulate concentration') ! no need for vertical_movement, minimum_value or other things because it is register_state_DEPENDENCY (see base_model for the difference and the fabm.yaml file)
+    call self%register_state_dependency(self%id_d, 'd', 'mmolC m-3', 'dissolved concentration')
+   
   end subroutine initialize
 
   subroutine do(self, _ARGUMENTS_DO_)
